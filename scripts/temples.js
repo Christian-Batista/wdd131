b3353a9ff6cd99cff1ff63fa3a51e0790f86eac7
ChristianBatista <elcatchon@gmail.com>
2025-05-16 18:39:43 -0400

    Temple Gallery

---
+++ scripts/temples.js
@@ -0,0 +1,22 @@
+const toggleBtn = document.querySelector('.menu-toggle');
+const menu = document.querySelector('.menu');
+const closeBtn = document.querySelector('.close-button');
+const main = document.querySelector('main');
+
+// Toggle menu
+toggleBtn.addEventListener('click', () => {
+  menu.classList.toggle('active');
+
+  if (menu.classList.contains('active')) {
+    main.classList.add('menu-active-main');
+  }
+});
+
+// Close menu
+closeBtn.addEventListener('click', () => {
+  menu.classList.remove('active');
+
+  if (!menu.classList.contains('active')) {
+    main.classList.remove('menu-active-main');
+  }
+});
